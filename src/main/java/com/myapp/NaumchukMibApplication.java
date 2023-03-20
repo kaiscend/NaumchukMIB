274b6a1f6f49044052a6bcdb2d4dd33f96397da6
unknown <esnaum38@gmail.com>
2023-03-20 17:56:19 +0000

    first commit

---
+++ src/main/java/com/myapp/NaumchukMibApplication.java
@@ -0,0 +1,13 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  NaumchukMibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaumchukMibApplication.class, args);
+    }
+
+}
